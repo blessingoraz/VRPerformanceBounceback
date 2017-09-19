a14604881ddbcb05096329374f99041c831646bd
Jen Oslislo <poeksweb@gmail.com>
2017-09-18 19:02:25 -0700

    init

---
+++ Assets/PerformanceBounceback/Scripts/FPSViewer.cs
@@ -0,0 +1,92 @@
+﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
+//
+// Purpose: Helper to display various hmd stats via GUIText
+//
+//=============================================================================
+
+using UnityEngine;
+using UnityEngine.UI;
+using Valve.VR;
+
+public class FPSViewer : MonoBehaviour
+{
+    public Text text;
+
+    public Color fadeColor = Color.black;
+    public float fadeDuration = 1.0f;
+    private CVRCompositor compositor;
+
+    void Awake()
+    {
+        compositor = OpenVR.Compositor;
+
+        if (text == null)
+        {
+            text = GetComponent<Text>();
+            text.enabled = false;
+        }
+
+        if (fadeDuration > 0)
+        {
+            SteamVR_Fade.Start(fadeColor, 0);
+            SteamVR_Fade.Start(Color.clear, fadeDuration);
+        }
+    }
+
+    double lastUpdate = 0.0f;
+
+    void Update()
+    {
+        if (text != null)
+        {
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                text.enabled = !text.enabled;
+            }
+
+            if (text.enabled)
+            {
+                if (compositor != null)
+                {
+                    SetTiming();
+                }
+            }
+        }
+    }
+
+    void SetTiming()
+    {
+
+        var timing = new Compositor_FrameTiming();
+        timing.m_nSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(typeof(Compositor_FrameTiming));
+        compositor.GetFrameTiming(ref timing, 0);
+
+        var update = timing.m_flSystemTimeInSeconds;
+        if (update > lastUpdate)
+        {
+            var framerate = (lastUpdate > 0.0f) ? 1.0f / (update - lastUpdate) : 0.0f;
+            int numFramesDropped = (int)timing.m_nNumDroppedFrames;
+            lastUpdate = update;
+            text.text = string.Format("FPS: {0:N0}\nDropped: {1}", framerate, numFramesDropped);
+            ColorizeText(framerate);
+        }
+        else
+        {
+            lastUpdate = update;
+        }
+    }
+
+    void ColorizeText(double framerate)
+    {
+        text.color = Color.black;
+        if (framerate < 70)
+        {
+            text.color = Color.yellow;
+        }
+        if (framerate < 50)
+        {
+            text.color = Color.red;
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,21 @@
+# Performance Bounceback
+
+### Name ###
+
+Jen Oslislo
+
+### Headset ###
+
+Vive
+
+### Time Spent ###
+
+14 Hours
+
+### One thing I liked ###
+
+I enjoyed making modifications that had an appreciable effect on performance.
+
+### One thing I found challenging ###
+
+The lighting!
